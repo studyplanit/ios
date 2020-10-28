4a2e7393ea3bf976c0235165ad89a6c55d6d09ac
younoah <kr_uno_10@naver.com>
2020-10-28 21:48:50 +0900

    feat: Add Challenge & Event View Controller / 윤호

---
+++ Split/Plan/Controller/ChallengeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChallengeViewController.swift
+//  Split
+//
+//  Created by uno on 2020/10/28.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController {
+    
+    // MARK:- Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
