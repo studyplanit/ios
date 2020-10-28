98c156f3a4dba9b58761483e5efcbdbf643c1781
younoah <kr_uno_10@naver.com>
2020-10-28 20:56:02 +0900

    feat: Update category button / 윤호

--- Split/Plan/Controller/PlanViewController.swift
+++ Split/Plan/Controller/PlanViewController.swift
@@ -9,66 +9,20 @@ import UIKit
 
 class PlanViewController: UIViewController {
     
-    // MARK:- Properties
-    private let challengeButton = UIButton()
-    private let eventButton = UIButton()
+    // MARK: Properties
+    @IBOutlet weak var chellengeButton: UIButton!
+    @IBOutlet weak var eventButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
     
-    // MARK:- View Life Cycle
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        drawCategory()
     }
     
 }
 // MARk:- Configure UI
 extension PlanViewController {
-    
-    func drawCategory() {
-        challengeButton.translatesAutoresizingMaskIntoConstraints = false
-        challengeButton.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.1725490196, blue: 0.1725490196, alpha: 1)
-        challengeButton.setTitle("Challenge", for: .normal)
-        challengeButton.setTitleColor(#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1), for: .normal)
-        challengeButton.setTitleColor(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: .highlighted)
-        challengeButton.addTarget(
-            self,
-            action: #selector(touchUpChallengeButton),
-            for: .touchUpInside)
-        view.addSubview(challengeButton)
-        challengeButton.heightAnchor.constraint(
-            equalToConstant: 40).isActive = true
-        challengeButton.topAnchor.constraint(
-            equalTo: view.topAnchor,
-            constant: 0).isActive = true
-        challengeButton.leadingAnchor.constraint(
-            equalTo: view.leadingAnchor,
-            constant: 0).isActive = true
-        challengeButton.trailingAnchor.constraint(
-            equalTo: view.centerXAnchor,
-            constant: 0).isActive = true
-        
-        eventButton.translatesAutoresizingMaskIntoConstraints = false
-        eventButton.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.1725490196, blue: 0.1725490196, alpha: 1)
-        eventButton.setTitle("Event", for: .normal)
-        eventButton.setTitleColor(#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1), for: .normal)
-        eventButton.setTitleColor(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: .highlighted)
-        eventButton.addTarget(
-            self,
-            action: #selector(touchUpEventButton),
-            for: .touchUpInside)
-        view.addSubview(eventButton)
-        eventButton.heightAnchor.constraint(
-            equalToConstant: 40).isActive = true
-        eventButton.topAnchor.constraint(
-            equalTo: view.topAnchor,
-            constant: 0).isActive = true
-        eventButton.leadingAnchor.constraint(
-            equalTo: view.centerXAnchor,
-            constant: 0).isActive = true
-        eventButton.trailingAnchor.constraint(
-            equalTo: view.trailingAnchor,
-            constant: 0).isActive = true
-    }
     
 }
 
