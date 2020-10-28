953b7c04fe9dba4fc94348734f1829454e6582f9
younoah <kr_uno_10@naver.com>
2020-10-28 23:55:19 +0900

    feat: Add Event cell / 윤호

---
+++ Split/Plan/Controller/Cell/Event/EventBannerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventBannerTableViewCell.swift
+//  Split
+//
+//  Created by uno on 2020/10/28.
+//
+
+import UIKit
+
+class EventBannerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Split/Plan/Controller/EventViewController.swift
+++ Split/Plan/Controller/EventViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class EventViewController: UIViewController {
-
+    
+    // MARK:- Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK:- Configure
+extension EventViewController {
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-    */
+    
+}
+
+// MARK:- Methods
+extension EventViewController {
+    
+}
+
+// MARK:- Table View DataSource
+extension EventViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
+
+// MARK:- Table View Delegate
+extension EventViewController: UITableViewDelegate {
+    
+}
+
