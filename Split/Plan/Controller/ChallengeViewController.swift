5282d3b60a53fcede94e9d8261c158802c64947e
younoah <kr_uno_10@naver.com>
2020-10-28 23:39:26 +0900

    feat: Add Challenge & Event subViews / 윤호

--- Split/Plan/Controller/ChallengeViewController.swift
+++ Split/Plan/Controller/ChallengeViewController.swift
@@ -15,8 +15,93 @@ class ChallengeViewController: UIViewController {
     // MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
     }
+    
+}
 
+// MARK:- Configure
+extension ChallengeViewController {
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(
+            UINib(nibName: "ChallengeBannerTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "challengeBannerTableViewCell")
+        tableView.register(
+            UINib(nibName: "ChallengeContentTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "challengeContentTableViewCell")
+    }
+    
+}
+
+extension ChallengeViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 4
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sectionIndex = indexPath.section
+        
+        switch sectionIndex {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: "challengeBannerTableViewCell",
+                    for: indexPath) as? ChallengeBannerTableViewCell else { return UITableViewCell() }
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: "challengeContentTableViewCell",
+                    for: indexPath) as? ChallengeContentTableViewCell else { return UITableViewCell() }
+            switch indexPath.row {
+            case 0:
+                cell.planTitleLabel.text = "출석 챌린지 플랜 1일"
+                return cell
+            case 1:
+                cell.planTitleLabel.text = "출석 챌린지 플랜 7일"
+                return cell
+            case 2:
+                cell.planTitleLabel.text = "출석 챌린지 플랜 15일"
+                return cell
+            case 3:
+                cell.planTitleLabel.text = "출석 챌린지 플랜 30일"
+                return cell
+            default:
+                return UITableViewCell()
+            }
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+}
+
+extension ChallengeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let screenWidth = UIScreen.main.bounds.size.width
+        switch indexPath.section {
+        case 0:
+            return screenWidth * 3/4
+        case 1:
+            return 100
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
 }
--- Split/Plan/Controller/PlanViewController.swift
+++ Split/Plan/Controller/PlanViewController.swift
@@ -10,30 +10,99 @@ import UIKit
 class PlanViewController: UIViewController {
     
     // MARK: Properties
-    @IBOutlet weak var chellengeButton: UIButton!
-    @IBOutlet weak var eventButton: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var contentView: UIView!
+    
+    private lazy var challengeViewController: ChallengeViewController = {
+        let storyboard = UIStoryboard(name: "Plan", bundle: Bundle.main)
+        var viewController = storyboard.instantiateViewController(withIdentifier: "challengeViewController") as! ChallengeViewController
+        // Add View Controller as Child View Controller
+        self.addView(asChildViewController: viewController)
+        return viewController
+    }()
+    private lazy var eventViewController: EventViewController = {
+        let storyboard = UIStoryboard(name: "Plan", bundle: Bundle.main)
+        var viewController = storyboard.instantiateViewController(withIdentifier: "eventViewController") as! EventViewController
+        // Add View Controller as Child View Controller
+        self.addView(asChildViewController: viewController)
+        return viewController
+    }()
     
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureTapBar()
+        configureContentView()
     }
     
 }
-// MARk:- Configure UI
+// MARk:- Configure
 extension PlanViewController {
+    
+    func configureTapBar() {
+        navigationItem.title = "플랜"
+    }
+    
+    func configureSegementedControl() {
+        segmentedControl.removeAllSegments()
+        segmentedControl.insertSegment(withTitle: "Challenge", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "Event", at: 1, animated: false)
+        segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
+
+        // Select First Segment
+        segmentedControl.selectedSegmentIndex = 0
+    }
+    
+    func configureContentView() {
+        configureSegementedControl()
+        updateView()
+    }
     
 }
 
 // MARk:- Methods
 extension PlanViewController {
     
-    @objc func touchUpChallengeButton() {
-        print("challenge button clicked")
+    @objc func selectionDidChange(_ sender: UISegmentedControl) {
+        updateView()
     }
     
-    @objc func touchUpEventButton() {
-        print("event button clicked")
+    private func addView(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChild(viewController)
+
+        // Add Child View as Subview
+        contentView.addSubview(viewController.view)
+
+        // Configure Child View
+        viewController.view.frame = contentView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
     }
+    
+    private func removeView(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParent: nil)
+
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+
+        // Notify Child View Controller
+        viewController.removeFromParent()
+    }
+    
+    private func updateView() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            removeView(asChildViewController: eventViewController)
+            addView(asChildViewController: challengeViewController)
+        } else {
+            removeView(asChildViewController: challengeViewController)
+            addView(asChildViewController: eventViewController)
+        }
+    }
+    
+    
 }
