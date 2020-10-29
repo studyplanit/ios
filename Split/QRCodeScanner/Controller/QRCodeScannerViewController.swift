d71052f0516ae596721454d232b2e32f1b4f46d7
younoah <kr_uno_10@naver.com>
2020-10-29 22:55:25 +0900

    feat: Add QRCodeScanner ViewController & stryboard / 윤호

---
+++ Split/QRCodeScanner/Controller/QRCodeScannerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  QRCodeScannerViewController.swift
+//  Split
+//
+//  Created by uno on 2020/10/29.
+//
+
+import UIKit
+
+class QRCodeScannerViewController: UIViewController {
+    
+    // MARK:- Properties
+    
+    // MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+extension QRCodeScannerViewController {
+    
+}
