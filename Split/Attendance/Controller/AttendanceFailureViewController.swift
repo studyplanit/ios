675d166d520788e31fce1fdd745af5b6d427e981
younoah <kr_uno_10@naver.com>
2020-11-09 19:40:08 +0900

    feat: add AttendanceFailureViewController / 윤호

---
+++ Split/Attendance/Controller/AttendanceFailureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AttendanceFailureViewController.swift
+//  Split
+//
+//  Created by uno on 2020/11/09.
+//
+
+import UIKit
+
+class AttendanceFailureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
